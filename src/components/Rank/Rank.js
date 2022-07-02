4ec4c65887efa4e658fde17c39275482b86883fc
miriam-best-practice <nakiyingim@gmail.com>
2022-07-02 16:40:47 -0700

    add rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'Miriam your Rank is  .....'}
+      </div>
+      <div className='white f1'>
+        {'#5'}
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Rank
